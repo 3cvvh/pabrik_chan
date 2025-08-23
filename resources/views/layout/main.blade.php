d89499e06d8435797ef70a874c7761954e43a27a
Rasyaalfaris <mrhaski632@gmail.com>
2025-08-23 12:50:01 +0700

    pavicon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,12 @@
     <title>{{ $judul }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/pavicon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/pavicon.png') }}">
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('img/pavicon.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/pavicon.png') }}">
+
+
 </head>
 <body>
     @yield('content')
